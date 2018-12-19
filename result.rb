1943c758a2ffb71de8959ef7501881a5f0025c92
Naoya Niwa <mine@mine-studio.com>
2018-12-19 09:48:51 +0900

    Fix time measurement method

--- result.rb
+++ result.rb
@@ -7,25 +7,21 @@ OUTPUT_DIR = "cases"
 def main
   lognames = Dir.glob(OUTPUT_DIR + "/link*.log")
   CSV.open("result.csv", "wb") do |csv|
-    csv << ["bandwidth", "latency", "core", "stime", "atime"]
+    csv << ["bandwidth", "latency", "core", "time"]
     lognames.each do |logname|
       s = File.basename(logname, ".*").split("_")
       band = s[1]
       latency = s[2]
       core = /core([0-9]+)/.match(s[3])[1]
-      stime = 0
-      atime = 0
+      time = 0
       File.open(logname, "r") do |f|
         f.each_line do |line|
-          if line.include?("Simulated time:")
-            stime = /(\S+) seconds/.match(line)[1]
-          end
-          if line.include?("seconds were actual computation of the application")
-            atime = /(\S+) seconds/.match(line)[1]
+          if line.include?("Final Distance")
+            time = /\((\S+)\)/.match(line)[1]
           end
         end
       end
-      csv << [band, latency, core, stime, atime]
+      csv << [band, latency, core, time]
     end
   end
 end
